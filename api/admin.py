a43a2fd43e505c6a753e7ebca1b7db22a7abf300
Eric G <eric.gichuri@andela.com>
2016-01-07 11:17:32 +0300

    (Feature) Add models and configure admin
    Created Bucketlist and Item models.
    Added admin user.
    Configured display of bucketlist and bucketlist items in the
    backend
    Performed migrations

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Bucketlist, Item
 
-# Register your models here.
+
+class BucketlistAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'date_created', 'date_modified', 'created_by'
+    )
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'date_created', 'date_modified', 'done', 'bucketlist'
+    )
+
+
+admin.site.register(Bucketlist, BucketlistAdmin)
+
+admin.site.register(Item, ItemAdmin)
